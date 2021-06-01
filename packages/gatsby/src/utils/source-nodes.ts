f496046deca6f2674dd6226d09dd1e2deb4528dd
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2021-06-02 05:17:33 +0700

    replace getNode and getNodeByType usages with iterable alternatives

--- packages/gatsby/src/utils/source-nodes.ts
+++ packages/gatsby/src/utils/source-nodes.ts
@@ -2,26 +2,33 @@ import report from "gatsby-cli/lib/reporter"
 import { Span } from "opentracing"
 import apiRunner from "./api-runner-node"
 import { store } from "../redux"
-import { getDataStore, getNode, getNodes } from "../datastore"
+import { getDataStore, getNode, isLmdbStore } from "../datastore"
 import { actions } from "../redux/actions"
-import { IGatsbyState } from "../redux/types"
+import { IGatsbyNode } from "../redux/types"
+import { IGatsbyIterable } from "../datastore/types"
+
 const { deleteNode } = actions
-import { Node } from "../../index"
 
 /**
  * Finds the name of all plugins which implement Gatsby APIs that
  * may create nodes, but which have not actually created any nodes.
  */
-function discoverPluginsWithoutNodes(
-  storeState: IGatsbyState,
-  nodes: Array<Node>
-): Array<string> {
+function discoverPluginsWithoutNodes(): Array<string> {
+  if (isLmdbStore() && getDataStore().countNodes() > 100000) {
+    // This method is pretty expensive, especially with lmdb-store
+    // TODO: count the number of nodes per plugin in reducers instead
+    return []
+  }
+
   // Find out which plugins own already created nodes
   const nodeOwnerSet = new Set([`default-site-plugin`])
-  nodes.forEach(node => nodeOwnerSet.add(node.internal.owner))
+  getDataStore()
+    .iterateNodes()
+    .forEach(node => nodeOwnerSet.add(node.internal.owner))
 
-  return storeState.flattenedPlugins
-    .filter(
+  return store
+    .getState()
+    .flattenedPlugins.filter(
       plugin =>
         // "Can generate nodes"
         plugin.nodeAPIs.includes(`sourceNodes`) &&
@@ -34,11 +41,8 @@ function discoverPluginsWithoutNodes(
 /**
  * Warn about plugins that should have created nodes but didn't.
  */
-function warnForPluginsWithoutNodes(
-  state: IGatsbyState,
-  nodes: Array<Node>
-): void {
-  const pluginsWithNoNodes = discoverPluginsWithoutNodes(state, nodes)
+function warnForPluginsWithoutNodes(): void {
+  const pluginsWithNoNodes = discoverPluginsWithoutNodes()
 
   pluginsWithNoNodes.map(name =>
     report.warn(
@@ -50,39 +54,40 @@ function warnForPluginsWithoutNodes(
 /**
  * Return the set of nodes for which its root node has not been touched
  */
-function getStaleNodes(state: IGatsbyState, nodes: Array<Node>): Array<Node> {
-  return nodes.filter(node => {
-    let rootNode = node
-    let next: Node | undefined = undefined
+function getStaleNodes(): IGatsbyIterable<IGatsbyNode> {
+  const state = store.getState()
 
-    let whileCount = 0
-    do {
-      next = rootNode.parent ? getNode(rootNode.parent) : undefined
-      if (next) {
-        rootNode = next
+  return getDataStore()
+    .iterateNodes()
+    .filter(node => {
+      let rootNode = node
+      let next: IGatsbyNode | undefined = undefined
+
+      let whileCount = 0
+      do {
+        next = rootNode.parent ? getNode(rootNode.parent) : undefined
+        if (next) {
+          rootNode = next
+        }
+      } while (next && ++whileCount < 101)
+
+      if (whileCount > 100) {
+        console.log(
+          `It looks like you have a node that's set its parent as itself`,
+          rootNode
+        )
       }
-    } while (next && ++whileCount < 101)
 
-    if (whileCount > 100) {
-      console.log(
-        `It looks like you have a node that's set its parent as itself`,
-        rootNode
-      )
-    }
-
-    return !state.nodesTouched.has(rootNode.id)
-  })
+      return !state.nodesTouched.has(rootNode.id)
+    })
 }
 
 /**
  * Find all stale nodes and delete them
  */
-function deleteStaleNodes(state: IGatsbyState, nodes: Array<Node>): void {
-  const staleNodes = getStaleNodes(state, nodes)
-
-  if (staleNodes.length > 0) {
-    staleNodes.forEach(node => store.dispatch(deleteNode(node)))
-  }
+function deleteStaleNodes(): void {
+  const staleNodes = getStaleNodes()
+  staleNodes.forEach(node => store.dispatch(deleteNode(node)))
 }
 
 export default async ({
@@ -106,10 +111,6 @@ export default async ({
   })
   await getDataStore().ready()
 
-  const state = store.getState()
-  const nodes = getNodes()
-
-  warnForPluginsWithoutNodes(state, nodes)
-
-  deleteStaleNodes(state, nodes)
+  warnForPluginsWithoutNodes()
+  deleteStaleNodes()
 }
