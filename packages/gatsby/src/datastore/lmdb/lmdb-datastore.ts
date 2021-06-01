7fd9b379f8c971ef07cea5856af039bd30d20150
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2021-06-02 03:45:27 +0700

    add iterable alternatives to getNodes and getNodesByType

--- packages/gatsby/src/datastore/lmdb/lmdb-datastore.ts
+++ packages/gatsby/src/datastore/lmdb/lmdb-datastore.ts
@@ -1,9 +1,9 @@
-import { ArrayLikeIterable, RootDatabase, open } from "lmdb-store"
+import { RootDatabase, open } from "lmdb-store"
 // import { performance } from "perf_hooks"
 import { ActionsUnion, IGatsbyNode } from "../../redux/types"
 import { updateNodes } from "./updates/nodes"
 import { updateNodesByType } from "./updates/nodes-by-type"
-import { IDataStore, ILmdbDatabases } from "../types"
+import { IDataStore, IGatsbyIterable, ILmdbDatabases } from "../types"
 import { emitter, replaceReducer } from "../../redux"
 
 const rootDbFile =
@@ -43,6 +43,9 @@ function getDatabases(): ILmdbDatabases {
   return databases
 }
 
+/**
+ * @deprecated
+ */
 function getNodes(): Array<IGatsbyNode> {
   // const start = performance.now()
   const result = Array.from<IGatsbyNode>(iterateNodes())
@@ -54,6 +57,9 @@ function getNodes(): Array<IGatsbyNode> {
   return result ?? []
 }
 
+/**
+ * @deprecated
+ */
 function getNodesByType(type: string): Array<IGatsbyNode> {
   // const start = performance.now()
   const result = Array.from<IGatsbyNode>(iterateNodesByType(type))
@@ -65,7 +71,7 @@ function getNodesByType(type: string): Array<IGatsbyNode> {
   return result ?? []
 }
 
-function iterateNodes(): ArrayLikeIterable<IGatsbyNode> {
+function iterateNodes(): IGatsbyIterable<IGatsbyNode> {
   // Additionally fetching items by id to leverage lmdb-store cache
   const nodesDb = getDatabases().nodes
   return nodesDb
@@ -74,7 +80,7 @@ function iterateNodes(): ArrayLikeIterable<IGatsbyNode> {
     .filter(Boolean)
 }
 
-function iterateNodesByType(type: string): ArrayLikeIterable<IGatsbyNode> {
+function iterateNodesByType(type: string): IGatsbyIterable<IGatsbyNode> {
   const nodesByType = getDatabases().nodesByType
   return nodesByType
     .getValues(type)
